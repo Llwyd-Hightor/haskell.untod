8b703a7c0eaac741754dd9e3e65e8c32c05bddad
Brent Longborough <brent@longborough.org>
2020-07-17 19:21:39 +0100

    Change default for mode DATE to current Local time

--- src/Main.hs
+++ src/Main.hs
@@ -12,7 +12,8 @@ import System.Environment (
     )
 import Data.Time (
     timeZoneMinutes
-  , getCurrentTime
+  , getZonedTime
+  , zonedTimeToUTC
   , formatTime
   , getCurrentTimeZone
   , defaultTimeLocale
@@ -61,7 +62,7 @@ main = do
     lSysZone <- fmap timeZoneMinutes getCurrentTimeZone
     utClip   <- getClipboardString
     utInput  <- getInput $ input options
-    utNow    <- getCurrentTime
+    ltNow    <- getZonedTime
 
     let utwork = Uwork {
       aEnvZone = convZone aEnvZone
@@ -70,7 +71,7 @@ main = do
     , uInput   = alist options
                 ++ flatlines (nocomment (getClip (clip options) utClip))
                 ++ flatlines (nocomment utInput)
-    , uNow     = take 27 $ ftime "%F@%T%Q" utNow
+    , uNow     = take 27 $ formatTime defaultTimeLocale "%F@%T%Q" ltNow
     , tSep     = if csv options then [] else " :"
     , rSep     = if csv options then "," else " "
     , tAdj     = if tickmode options == TAI then 10 else 0
